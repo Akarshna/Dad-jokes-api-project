17dd516d1a0c6bfd03fbcf647b143d5b5f0d059e
Akarshna <90909144+Akarshna@users.noreply.github.com>
2021-09-21 16:16:31 +0530

    final commit
    This is one is made after seeing a video on youtube, they used this api.

---
+++ script.js
@@ -0,0 +1,27 @@
+// Promises
+
+// Promises are like real life promises either fulfilled or rejected
+
+const jokes = document.querySelector('#joke');
+const jokesBtn = document.querySelector("#joke-btn");
+
+generateJokes = () =>{
+
+    const setHeader = {
+        headers:{
+            Accept : "application/json"
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com', setHeader)
+    .then((res) => res.json())
+    .then((data) => {
+        jokes.innerHTML = data.joke;
+    }).catch((error) => {
+        console.log(error);
+    })
+
+}
+
+jokesBtn.addEventListener('click', generateJokes);
+generateJokes();
